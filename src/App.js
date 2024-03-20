51b8ba31ac9c4e8287c56f3d535592bbaea033f1
Romane Orlando Robb <mail@romanerobb.com>
2024-03-20 13:58:09 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Romane Orlando Robb
         </a>
       </header>
     </div>
