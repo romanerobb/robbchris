86bf9bdb6ce1e82f5984077d0906a112cc804b2c
Romane Orlando Robb <mail@romanerobb.com>
2024-03-20 14:04:36 -0400

    Updated

---
+++ src/components/login/login.jsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Footer from '../footer/footer';
+
+const theme = createTheme();
+
+function Login() {
+
+  const navigate = useNavigate();
+
+  const url = "https://rorobb.com"
+
+  const usernameInput = useRef();
+  const userpasswordInput = useRef();
+
+  async function login(){
+
+    const userprofile = {
+        
+        username: usernameInput.current.value,
+        userpassword: userpasswordInput.current.value,
+    }
+    
+    try{
+        const response = await axios.post(`${url}/auth`, userprofile)
+        console.log(response.data)
+        navigate("/dashboard");
+    } catch (error) {
+        console.error(error.response.data)
+        console.log(error)
+        alert(error.response.data);
+    }
+}
+
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random/?city,night)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <img src="https://i.ibb.co/nCcW6P1/JMR-70.jpg" alt="JMRodgers Logo" width="400" height="auto"></img>
+            <Avatar sx={{ m: 1, bgcolor: 'success.main' }}>
+            <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h9">
+              Sign in and start tracking
+            </Typography>
+
+            <Box component="form" onSubmit={login} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="success" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="success" 
+              onClick={login}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            </Box>
+            <Footer />
+          </Box>
+        </Grid>
+      </Grid>
+
+    </ThemeProvider>
+    </>
+  );
+}
+
+export default Login;
